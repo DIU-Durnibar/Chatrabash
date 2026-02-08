1b811acb9df671bf92d25694b8a5b00194e110bf
Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-02-09 03:16:53 +0600

    Added Crud Operations for Room Entity

--- chatrabash.server/API/Controllers/RoomsController.cs
+++ chatrabash.server/API/Controllers/RoomsController.cs
@@ -14,4 +14,35 @@ public class RoomsController (AppDbContext context) : BaseController
         var rooms = await context.Rooms.ToListAsync();
         return Ok(rooms);
     }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Room>> GetRoomById([FromRoute] string id)
+    {
+        var room = await context.Rooms.FindAsync(id);
+        if(room == null) return BadRequest();
+
+        return Ok(room);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateRoom([FromBody] Room room)
+    {
+        if(room == null) return BadRequest();
+        context.Add(room);
+        await context.SaveChangesAsync();
+        return Ok(room.Id); 
+    }
+
+
+    [HttpDelete]
+    public async Task<ActionResult> DeleteRoomById([FromRoute] string id)
+    {
+        var room = await context.Rooms.FindAsync(id);
+        if(room == null) return NotFound();
+        
+        context.Rooms.Remove(room);
+        await context.SaveChangesAsync();
+        return Ok("Deleted: " + id);
+    }
+
 }
