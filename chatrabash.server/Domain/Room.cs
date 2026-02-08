7c04c6b8eba12db33ad4a2bf09bbfdc7ebebc173
Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-02-09 01:43:30 +0600

    Added Room Entity on Domain

---
+++ chatrabash.server/Domain/Room.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Domain;
+
+public class Room
+{
+    public string Id { get; set; } = Guid.CreateVersion7().ToString();
+
+    public string RoomNumber { get; set; } = "Unknown"; 
+
+    public int FloorNo { get; set; }
+
+    public int SeatCapacity { get; set; } 
+
+    public int SeatAvailable { get; set}
+
+    public int IsAttachedBathroomAvailable {get; set;}
+
+    public int IsBalconyAvailable {get; set;}
+
+    public bool IsAcAvailable { get; set; } 
+
+    public bool IsActive { get; set; } = true; 
+        
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? LastModifiedAt { get; set; } 
+}
