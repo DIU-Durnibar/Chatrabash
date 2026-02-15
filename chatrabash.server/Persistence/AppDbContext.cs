254df676a5a3a5c782ead5a31749a9b4337ef0d7
Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-02-15 13:24:22 +0600

    Added EF Identity class User and created migration

--- chatrabash.server/Persistence/AppDbContext.cs
+++ chatrabash.server/Persistence/AppDbContext.cs
@@ -2,10 +2,11 @@ using System;
 using System.Dynamic;
 using Microsoft.EntityFrameworkCore;
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Persistence;
 
-public class AppDbContext(DbContextOptions options) : DbContext (options)
+public class AppDbContext(DbContextOptions options) : IdentityDbContext<User>(options)
 {
     public required DbSet<Room> Rooms { get; set; } 
 }
